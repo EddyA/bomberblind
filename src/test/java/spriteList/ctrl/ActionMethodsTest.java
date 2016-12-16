48f2ed66a93c3ca3f03cb9abcc887df9fbd1c847
Eddy ALBERT <ealbert@mediametrie.fr>
2016-12-16 09:47:14 +0100

    Fix a test in ActionMethodes.

--- src/test/java/spriteList/ctrl/ActionMethodsTest.java
+++ src/test/java/spriteList/ctrl/ActionMethodsTest.java
@@ -1,6 +1,7 @@
 package spriteList.ctrl;
 
 import java.io.IOException;
+import java.util.LinkedList;
 
 import org.assertj.core.api.WithAssertions;
 import org.junit.Before;
@@ -8,6 +9,8 @@ import org.junit.Test;
 import org.mockito.Mockito;
 
 import images.ImagesLoader;
+import map.MapPoint;
+import sprite.Sprite;
 import sprite.nomad.CloakedSkeleton;
 import sprite.nomad.Enemy;
 
@@ -20,12 +23,23 @@ public class ActionMethodsTest implements WithAssertions {
 
     @Test
     public void processEnemyShouldReturnTrueWhenTheEnemyIsFinished() throws Exception {
-        Enemy enemy = new CloakedSkeleton(1, 2);
+        int mapWidth = 2;
+        int mapHeight = 2;
+
+        // set map.
+        MapPoint[][] mapPointMatrix = new MapPoint[mapHeight][mapWidth];
+        for (int rowIdx = 0; rowIdx < mapHeight; rowIdx++) {
+            for (int colIdx = 0; colIdx < mapWidth; colIdx++) {
+                mapPointMatrix[rowIdx][colIdx] = new MapPoint(rowIdx, colIdx);
+            }
+        }
+
+        Enemy enemy = new CloakedSkeleton(15, 30);
         Enemy spyedEnemy = Mockito.spy(enemy);
         Mockito.when(spyedEnemy.isFinished()).thenReturn(true);
 
         // test (as the relative check is done at the begining of the function, the different parameters are not
         // instancied).
-        assertThat(ActionMethods.processEnemy(null, null, 0, 0, spyedEnemy)).isTrue();
+        assertThat(ActionMethods.processEnemy(null, mapPointMatrix, mapWidth, mapHeight, spyedEnemy)).isTrue();
     }
 }
